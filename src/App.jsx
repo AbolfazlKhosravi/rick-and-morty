f11357a807d00e4f8ab48b9290bae3fa3063575f
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2024-09-01 11:03:44 +0330

    use clean function for serch data

--- src/App.jsx
+++ src/App.jsx
@@ -8,45 +8,69 @@ import axios from "axios";
 // import { allCharacters } from "../data/data";
 
 function App() {
-  const [characters, setCatacters] = useState([]);
+  const [characters, setChatacters] = useState([]);
   const [isloading, setIsloading] = useState(false);
   const [query, setQuery] = useState("");
   const [selectId, setSelectId] = useState();
-  const [favourites,setFavourites]=useState([])
-  
+  const [favourites, setFavourites] = useState([]);
 
-  const isAddedToFavourites=favourites.map(fav=>fav.id).includes(selectId)
+  const isAddedToFavourites = favourites
+    .map((fav) => fav.id)
+    .includes(selectId);
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     async function fetchdata() {
       try {
         setIsloading(true);
         const { data } = await axios.get(
-          `https://rickandmortyapi.com/api/character?name=${query}`
+          `https://rickandmortyapi.com/api/character?name=${query}`,
+          { signal }
         );
-        setCatacters(data.results.slice(0, 4));
+
+        setChatacters(data.results.slice(0, 4));
       } catch (error) {
-        setCatacters([]);
-        toast.error(error.response.data.error);
+        if (!axios.isCancel()) {
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.error
+          ) {
+            toast.error(error.response.data.error);
+            setChatacters([]);
+          }
+        }
       } finally {
         setIsloading(false);
       }
     }
-    if (query.length < 3) {
-      setCatacters([]);
-      return;
-    }
+    // if (query.length < 3) {
+    //   setChatacters([]);
+    //   return;
+    // }
     fetchdata();
+    return () => controller.abort();
   }, [query]);
   return (
     <div className="app">
       <Navbar>
         <Search query={query} setQuery={setQuery} />
         <SearchResult numOfCharacters={characters ? characters.length : 0} />
-        <Favourites numOfFavourites={favourites.length}/>
+        <Favourites numOfFavourites={favourites.length} />
       </Navbar>
       <Main>
-        <CharacterList characters={characters} isloading={isloading} setSelectId={setSelectId} selectId={selectId} />
-        <CharacterDetail selectId={selectId} setFavourites={setFavourites} isAddedToFavourites={isAddedToFavourites}/>
+        <CharacterList
+          characters={characters}
+          isloading={isloading}
+          setSelectId={setSelectId}
+          selectId={selectId}
+        />
+        <CharacterDetail
+          selectId={selectId}
+          setFavourites={setFavourites}
+          isAddedToFavourites={isAddedToFavourites}
+        />
       </Main>
       <Toaster />
     </div>
