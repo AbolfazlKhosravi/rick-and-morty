734f8961e1b911eec1be95ff637c8bcfe28c47c7
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2024-09-01 11:30:32 +0330

    refactor code

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -6,11 +6,50 @@ import axios from "axios";
 import Loader from "./Loader";
 
 function CharacterDetail({ selectId, setFavourites, isAddedToFavourites }) {
-  const [character, setCharacter] = useState(null);
   const [isloading, setIsloading] = useState(false);
-  const [episodes, setEpisodes] = useState(null);
-  const [isloadingEpisodes, setIsloadingEpisodes] = useState(false);
+  const [character, setCharacter] = useState(null);
 
+  return (
+    <>
+      {isloading ? (
+        <Loader />
+      ) : selectId ? (
+        <div className={{ flex: 1 }}>
+          <CharacterSubInfo
+            character={character}
+            setFavourites={setFavourites}
+            isAddedToFavourites={isAddedToFavourites}
+            selectId={selectId}
+            setIsloading={setIsloading}
+            setCharacter={setCharacter}
+          />
+          <CharacterEpisodes character={character} />
+        </div>
+      ) : (
+        <div
+          style={{
+            color: "var(--slate-300)",
+            display: "flex",
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "1rem",
+          }}
+        >
+          Please select a character
+        </div>
+      )}
+    </>
+  );
+}
+const CharacterSubInfo = ({
+  selectId,
+  setIsloading,
+  setCharacter,
+  character,
+  isAddedToFavourites,
+  setFavourites,
+}) => {
   useEffect(() => {
     async function getCaracter() {
       try {
@@ -28,8 +67,53 @@ function CharacterDetail({ selectId, setFavourites, isAddedToFavourites }) {
     if (selectId) {
       getCaracter();
     }
-  }, [selectId]);
-
+  }, [selectId, setCharacter, setIsloading]);
+  return (
+    character && (
+      <div className="character-detail">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{character.gender === "Male" ? "👨🏻" : "👩🏻‍🦰"}</span>
+            <span>&nbsp;{character.name}</span>
+          </h3>
+          <div className="info">
+            <span
+              className={`status ${character.status === "Dead" && "red"}`}
+            ></span>
+            <span>&nbsp;{character.status}</span>
+            <span> - &nbsp;{character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last Khown Location : </p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            {isAddedToFavourites ? (
+              <p>already Added To The Favourites ✅</p>
+            ) : (
+              <button
+                onClick={() =>
+                  setFavourites((prevData) => [...prevData, character])
+                }
+                className="btn btn--primary"
+              >
+                Add to Favourits
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+const CharacterEpisodes = ({ character }) => {
+  const [episodes, setEpisodes] = useState(null);
+  const [isloadingEpisodes, setIsloadingEpisodes] = useState(false);
   useEffect(() => {
     async function getEpisodes() {
       try {
@@ -51,95 +135,33 @@ function CharacterDetail({ selectId, setFavourites, isAddedToFavourites }) {
       getEpisodes();
     }
   }, [character]);
-
   return (
-    <>
-      {isloading ? (
-        <Loader />
-      ) : character?.id & selectId ? (
-        <div className={{ flex: 1 }}>
-          <div className="character-detail">
-            <img
-              src={character.image}
-              alt={character.name}
-              className="character-detail__img"
-            />
-            <div className="character-detail__info">
-              <h3 className="name">
-                <span>{character.gender === "Male" ? "👨🏻" : "👩🏻‍🦰"}</span>
-                <span>&nbsp;{character.name}</span>
-              </h3>
-              <div className="info">
-                <span
-                  className={`status ${character.status === "Dead" && "red"}`}
-                ></span>
-                <span>&nbsp;{character.status}</span>
-                <span> - &nbsp;{character.species}</span>
+    <div className="character-episodes">
+      <div className="title">
+        <h2>List of Episodes:</h2>
+        <button>
+          <ArrowUpCircleIcon className="icon" />
+        </button>
+      </div>
+      <ul>
+        {isloadingEpisodes ? (
+          <Loader />
+        ) : episodes ? (
+          episodes.map((item, index) => (
+            <li key={item.id}>
+              <div>
+                {String(index + 1).padStart(2, "0")} {item.episode} :{" "}
+                <strong>{item.name}</strong>
               </div>
-              <div className="location">
-                <p>Last Khown Location : </p>
-                <p>{character.location.name}</p>
-              </div>
-              <div className="actions">
-                {isAddedToFavourites ? (
-                  <p>already Added To The Favourites ✅</p>
-                ) : (
-                  <button
-                    onClick={() =>
-                      setFavourites((prevData) => [...prevData, character])
-                    }
-                    className="btn btn--primary"
-                  >
-                    Add to Favourits
-                  </button>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="character-episodes">
-            <div className="title">
-              <h2>List of Episodes:</h2>
-              <button>
-                <ArrowUpCircleIcon className="icon" />
-              </button>
-            </div>
-            <ul>
-              {isloadingEpisodes ? (
-                <Loader />
-              ) : episodes ? (
-                episodes.map((item, index) => (
-                  <li key={item.id}>
-                    <div>
-                      {String(index + 1).padStart(2, "0")} {item.episode} :{" "}
-                      <strong>{item.name}</strong>
-                    </div>
-                    <div className="badge badge--secondary">
-                      {item.air_date}
-                    </div>
-                  </li>
-                ))
-              ) : (
-                ""
-              )}
-            </ul>
-          </div>
-        </div>
-      ) : (
-        <div
-          style={{
-            color: "var(--slate-300)",
-            display: "flex",
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "1rem",
-          }}
-        >
-          Please select a character
-        </div>
-      )}
-    </>
+              <div className="badge badge--secondary">{item.air_date}</div>
+            </li>
+          ))
+        ) : (
+          ""
+        )}
+      </ul>
+    </div>
   );
-}
+};
 
 export default CharacterDetail;
